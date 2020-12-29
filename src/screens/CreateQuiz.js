b4cf4b33523c525cfe967fb1b92f0f657608e3cb
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2020-12-29 11:25:01 +0530

    Add files via upload

---
+++ src/screens/CreateQuiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+export default function CreateQuiz() {
+ 
+    //component rendering
+    return (
+       <Text>Create a Quiz</Text>
+    );
+}
---
+++ src/screens/MyQuizes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+export default function MyQuizes() {
+ 
+    //component rendering
+    return (
+       <Text>list of my quizes </Text>
+    );
+}
